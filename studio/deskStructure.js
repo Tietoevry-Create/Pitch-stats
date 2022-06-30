30c2dcaf96bfe33730c74bf0c526e8324da94ed8
Ådne Forslund <adne.forslund@gmail.com>
2022-06-30 13:31:48 +0200

    add desk structure

---
+++ studio/deskStructure.js
@@ -0,0 +1,13 @@
+import S from "@sanity/desk-tool/structure-builder";
+
+export default () =>
+  S.list()
+    .title("Content")
+    .items([
+      S.listItem()
+        .title("Front Page")
+        .child(S.document().schemaType("frontPage").documentId("frontPage")),
+      ...S.documentTypeListItems().filter(
+        (listItem) => !["frontPage"].includes(listItem.getId())
+      ),
+    ]);
