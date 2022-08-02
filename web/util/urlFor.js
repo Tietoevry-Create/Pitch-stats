a3c4db2efc1bd3e1fba947cc4c2f55d1977b5be3
Ådne Forslund <adne.forslund@gmail.com>
2022-08-02 15:04:32 +0200

    add urlFor function for images

---
+++ web/util/urlFor.js
@@ -0,0 +1,10 @@
+import imageUrlBuilder from "@sanity/image-url";
+import client from "./client";
+
+const builder = imageUrlBuilder(client);
+
+function urlFor(source) {
+  return builder.image(source);
+}
+
+export default urlFor;
