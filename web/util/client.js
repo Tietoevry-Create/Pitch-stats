b1a8452287aa00e88c0fc9cb6348276224f7a039
Ådne Forslund <adne.forslund@gmail.com>
2022-08-02 14:58:54 +0200

    add client

---
+++ web/util/client.js
@@ -0,0 +1,10 @@
+import sanityClient from "@sanity/client";
+
+const client = sanityClient({
+  projectId: "ts5bhhuv",
+  dataset: "production",
+  useCdn: false, // `false` if you want to ensure fresh data
+  apiVersion: "2022-01-31",
+});
+
+export default client;
