f9ef4535041d1e7caae337b17268ed78db0f3767
Ådne Forslund <adne.forslund@gmail.com>
2022-08-10 15:01:55 +0200

    add block compo

---
+++ web/components/blockContent.jsx
@@ -0,0 +1,11 @@
+import { PortableText } from "@portabletext/react";
+
+
+const BlockContent = ({blockContent, ...rest}) => (
+    
+    <PortableText value={blockContent} />
+   
+    
+  );
+  
+  export default  BlockContent;
