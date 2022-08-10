3dc65378d961749b0d3bf1bcaa5a1b981ff8d041
Ådne Forslund <adne.forslund@gmail.com>
2022-08-10 16:16:38 +0200

    add karla font

--- web/pages/_document.js
+++ web/pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="no">
-      <Head />
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Karla"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
