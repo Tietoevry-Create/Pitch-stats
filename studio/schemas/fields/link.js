b84a297f724929e1cc5428b1fee3dcf3a37fe144
Ådne Forslund <adne.forslund@gmail.com>
2022-08-17 13:50:34 +0200

    add link field

---
+++ studio/schemas/fields/link.js
@@ -0,0 +1,15 @@
+export default {
+  name: "link",
+  title: "Link",
+  type: "object",
+  fields: [
+    {
+      title: "Label",
+      name: "label",
+      description: "The visible label of the link.",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    },
+    { type: "url", title: "URL", name: "href" },
+  ],
+};
