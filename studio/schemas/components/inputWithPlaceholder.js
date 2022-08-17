3516237d3386d8abcac0bb58c063460b2438dcbe
Ådne Forslund <adne.forslund@gmail.com>
2022-08-17 15:12:55 +0200

    add new custom input

--- studio/schemas/components/inputWithPlaceholder.js
+++ studio/schemas/components/inputWithPlaceholder.js
@@ -1,15 +1,54 @@
 import React from "react";
+import { FormField } from "@sanity/base/components";
+import { TextInput } from "@sanity/ui";
+import PatchEvent, { set, unset } from "@sanity/form-builder/PatchEvent";
 
-import { Stack, Heading } from "@sanity/ui";
+import { useId } from "@reach/auto-id";
 
-const Placeholder = React.forwardRef((props, _ref) => {
+const InputWithPlaceholder = React.forwardRef((props, ref) => {
+  const {
+    type,
+    value,
+    readOnly,
+    placeholder,
+    markers,
+    presence,
+    compareValue,
+    onFocus,
+    onBlur,
+    onChange,
+  } = props;
+
+  const inputId = useId();
+
+  const handleChange = React.useCallback(
+    (event) => {
+      const inputValue = event.currentTarget.value;
+
+      onChange(PatchEvent.from(inputValue ? set(inputValue) : unset()));
+    },
+    [onChange]
+  );
+
   return (
-    <Stack space={2}>
-      <Heading as="h3" size={0}>
-        {props.type.title}
-      </Heading>
-    </Stack>
+    <FormField
+      description={type.description}
+      title={type.title}
+      __unstable_markers={markers}
+      __unstable_presence={presence}
+      compareValue={compareValue}
+      inputId={inputId}
+    >
+      <TextInput
+        id={inputId}
+        onChange={handleChange}
+        value={value}
+        readOnly={readOnly}
+        placeholder={placeholder}
+        onBlur={onBlur}
+      />
+    </FormField>
   );
 });
 
-export default Placeholder;
+export default InputWithPlaceholder;
