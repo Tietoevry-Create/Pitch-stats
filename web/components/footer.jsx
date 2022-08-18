5861090b954b48288fd73c09c02665d81335cf10
torbjornhusebo <torbjorn.husebo@evry.com>
2022-08-18 17:50:43 +0200

    Created basic layout for the footer

--- web/components/footer.jsx
+++ web/components/footer.jsx
@@ -1,18 +1,12 @@
-import Image from "next/image";
 const Footer = () => (
-  <footer className="bg-purple-800">
-  <div className="container mx-auto">
-    <a
-      href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Powered by{" "}
-      <span>
-        <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-      </span>
-    </a>
-    </div>
+  <footer className="bg-purple-800 absolute bottom-0 w-full">
+  <div className="container grid grid-rows-5 grid-flow-col gap-4 py-10 px-10 md:px-32 text-pink-100">
+    <p>Om dette prosjektet</p>
+		<p>Kildekode</p>
+		<p>Tietoevry</p>
+		<div className="mt-7 h-0.5 bg-pink-100 w-full"></div>
+		<p>©Tietoevry 2022</p>
+	</div>
   </footer>
 );
 
