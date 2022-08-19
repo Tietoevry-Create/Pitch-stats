1c1625b25400ae30fc015dfd16079fc1a25c04d6
torbjornhusebo <torbjorn.husebo@evry.com>
2022-08-19 20:48:24 +0200

    Did a basic setup for a navbar

--- web/components/layout.jsx
+++ web/components/layout.jsx
@@ -1,9 +1,11 @@
 import Footer from "./footer";
+import Navbar from "./navbar";
 const Layout = ({children, ...rest}) => (
     <>
+      <Navbar />
     <div className="container mx-auto">{children}</div>
     <Footer />
     </>
   );
-  
+
   export default Layout;
