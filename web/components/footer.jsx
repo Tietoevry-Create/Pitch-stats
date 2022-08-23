fc856afe5967da07f1b2c2c4a656f4c1b8f8e6f3
torbjornhusebo <torbjorn.husebo@evry.com>
2022-08-23 15:04:45 +0200

    Small update for PR review + layout adjustment navbar and footer

--- web/components/footer.jsx
+++ web/components/footer.jsx
@@ -1,11 +1,11 @@
 const Footer = () => (
   <footer className="bg-purple-800 absolute bottom-0 w-full">
-  <div className="container grid grid-rows-5 grid-flow-col gap-4 py-10 px-10 md:px-32 text-pink-100">
-    <p>Om dette prosjektet</p>
-		<p>Kildekode</p>
-		<p>Tietoevry</p>
-		<div className="mt-7 h-0.5 bg-pink-100 w-full"></div>
-		<p>©Tietoevry 2022</p>
+  <div className="grid grid-rows-5 grid-flow-col gap-4 py-10 px-10 md:px-52 text-pink-100">
+    <p className="px-3">Om dette prosjektet</p>
+		<p className="px-3">Kildekode</p>
+		<p className="px-3">Tietoevry</p>
+		<div className="mx-3 mt-7 h-0.5 bg-pink-100 w-full"></div>
+		<p className="px-3">©Tietoevry 2022</p>
 	</div>
   </footer>
 );
--- web/components/navbar.jsx
+++ web/components/navbar.jsx
@@ -2,23 +2,20 @@ import React from "react";
 import Link from "next/link";
 
 const Navbar = () => (
-	<>
-		<nav className="px-10 py-3 md:px-32 flex items-center flex-wrap bg-yellow-50">
-
-			<Link href='/'>
-				<a className='inline-flex items-center p-2 mr-4'>
-					<svg
-						viewBox='0 0 24 24'
-						xmlns='http://www.w3.org/2000/svg'
-						className='fill-current text-black h-8 w-8 mr-2'
-					>
-						<path d='M3 10v11h6v-7h6v7h6v-11L12,3z'/>
-					</svg>
-					<span className='text-xl text-black font-bold tracking-wide'>
-              photo
-            </span>
-				</a>
-			</Link>
+	<nav className="flex items-center flex-wrap bg-yellow-50">
+		<div className="px-10 py-3 md:px-52 w-full">
+			<span className='inline-flex items-center p-2 mr-4'>
+				<svg
+					viewBox='0 0 24 24'
+					xmlns='http://www.w3.org/2000/svg'
+					className='fill-current text-black h-8 w-8 mr-2'
+				>
+					<path d='M3 10v11h6v-7h6v7h6v-11L12,3z'/>
+				</svg>
+				<span className='text-xl text-black font-bold tracking-wide'>
+						photo
+					</span>
+			</span>
 
 			{/*		Can maybe use later
 			<button className=' inline-flex p-3 hover:bg-purple-800 rounded lg:hidden text-black ml-auto hover:text-pink-100 outline-none'>
@@ -40,30 +37,22 @@ const Navbar = () => (
 			*/}
 
 			<div className='lg:inline-flex lg:flex-row lg:w-auto lg:items-center lg:h-auto'>
-				<Link href='/'>
-					<a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-black font-bold items-center justify-center hover:bg-purple-800 hover:text-white'>
-						Features
-					</a>
-				</Link>
-				<Link href='/'>
-					<a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-black font-bold items-center justify-center hover:bg-purple-800 hover:text-white'>
-						Sites
-					</a>
-				</Link>
-				<Link href='/'>
-					<a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-black font-bold items-center justify-center hover:bg-purple-800 hover:text-white'>
-						About
-					</a>
-				</Link>
-				<Link href='/'>
-					<a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-black font-bold items-center justify-center hover:bg-purple-800 hover:text-white'>
-						 Tietoevry
-					</a>
-				</Link>
+				<span  className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-black font-bold items-center justify-center hover:bg-purple-800 hover:text-white'>
+					Features
+				</span>
+				<span className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-black font-bold items-center justify-center hover:bg-purple-800 hover:text-white'>
+					Sites
+				</span>
+				<span className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-black font-bold items-center justify-center hover:bg-purple-800 hover:text-white'>
+					About
+				</span>
+				<span className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-black font-bold items-center justify-center hover:bg-purple-800 hover:text-white'>
+					 Tietoevry
+				</span>
 			</div>
-			<div className="mt-3 h-0.5 bg-purple-800 w-full"></div>
-		</nav>
-	</>
+			<div className="mx-3 mt-3 h-0.5 bg-purple-800 w-full"></div>
+		</div>
+	</nav>
 );
 
 export default Navbar;
