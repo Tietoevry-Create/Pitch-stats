a3063e2e160e44f0708f862acc7014907bee0f62
Ådne Forslund <adne.forslund@gmail.com>
2022-08-25 14:30:17 +0200

    add styling to rich text

--- web/components/blockContent.jsx
+++ web/components/blockContent.jsx
@@ -1,9 +1,9 @@
 import { PortableText } from '@portabletext/react';
 
 const BlockContent = ({ blockContent, ...rest }) => (
-  <>
+  <div className="container mx-auto px-4 py-4">
     <PortableText value={blockContent} />
-  </>
+  </div>
 );
 
 export default BlockContent;
