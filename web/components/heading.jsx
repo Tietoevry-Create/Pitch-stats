41f1fb19c7e242a98863c32f9394117cd4cf79dd
Ådne Forslund <adne.forslund@gmail.com>
2022-08-25 14:32:53 +0200

    create heading compo

---
+++ web/components/heading.jsx
@@ -0,0 +1,9 @@
+const Layout = ({ title, ...rest }) => (
+    <div className="bg-tepurple  w-full">
+       <heading className="container mx-auto">
+        <h1>Title</h1>
+       </heading>
+    </div>
+  );
+  
+  export default Layout;
