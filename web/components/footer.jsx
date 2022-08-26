50043942d391a934816ead5ec1d24c0103226042
Ådne Forslund <adne.forslund@gmail.com>
2022-08-26 17:06:28 +0200

    remove absolute pos for footer

--- web/components/footer.jsx
+++ web/components/footer.jsx
@@ -1,6 +1,6 @@
 import CustomLink from "./customLink";
 const Footer = () => (
-  <footer className="bg-tepurple absolute bottom-0 w-full">
+  <footer className="bg-tepurple bottom-0 w-full">
   	<div className="mx-auto container grid grid-rows-5 grid-flow-col gap-4 py-10 px-4 md:px-20 text-pink-100">
 			<CustomLink classes="px-3 text-white hover:underline text-lg" label={"Om dette prosjektet"}/>
 			<CustomLink classes="px-3 color-white hover:underline text-lg" label={"Kildekode"}/>
