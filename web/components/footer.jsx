90ef74491ff37bbb171e1936730c619269d76a38
Ådne Forslund <adne.forslund@gmail.com>
2022-08-26 15:24:09 +0200

    bigger font in footer

--- web/components/footer.jsx
+++ web/components/footer.jsx
@@ -2,9 +2,9 @@ import CustomLink from "./customLink";
 const Footer = () => (
   <footer className="bg-tepurple absolute bottom-0 w-full">
   	<div className="mx-auto container grid grid-rows-5 grid-flow-col gap-4 py-10 px-4 md:px-20 text-pink-100">
-			<CustomLink classes="px-3 text-white hover:underline" label={"Om dette prosjektet"}/>
-			<CustomLink classes="px-3 color-white hover:underline" label={"Kildekode"}/>
-			<CustomLink classes="px-3 color-white hover:underline" label={"Tietoevry"}/>
+			<CustomLink classes="px-3 text-white hover:underline text-lg" label={"Om dette prosjektet"}/>
+			<CustomLink classes="px-3 color-white hover:underline text-lg" label={"Kildekode"}/>
+			<CustomLink classes="px-3 color-white hover:underline text-lg" label={"Tietoevry"}/>
 			<hr className="mx-3 mt-7 h-0.5 bg-pink-100 w-full" />
 			<p className="px-3">©Tietoevry 2022</p>
 		</div>
