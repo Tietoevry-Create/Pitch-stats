2d8abd0396dc2cdc39816c9ecc0f0cb756c1325b
Ådne Forslund <adne.forslund@gmail.com>
2022-08-26 17:06:13 +0200

    add customs heights to config

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -4,6 +4,16 @@ module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      height: {
+        100: '25rem',
+        104: '26rem',
+        108: '27rem',
+        112: '28rem',
+        116: '29rem',
+        120: '30rem',
+        124: '31rem',
+        128: '32rem'
+      },
       colors: { coffe: '#FBF0E7', tepurple: '#560E62' },
       fontFamily: {
         sans: ['Karla', ...defaultTheme.fontFamily.sans]
