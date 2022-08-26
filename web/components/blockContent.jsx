f2e2cc618d32ce261546807d710495dcd67c750c
Ådne Forslund <adne.forslund@gmail.com>
2022-08-26 14:37:43 +0200

    adjust portable text

--- web/components/blockContent.jsx
+++ web/components/blockContent.jsx
@@ -1,7 +1,7 @@
 import { PortableText } from '@portabletext/react';
 
 const BlockContent = ({ blockContent, ...rest }) => (
-  <div className="container mx-auto px-4 md:px-20 py-4">
+  <div className="container mx-auto px-4 md:px-40 py-10 text-lg">
     <PortableText value={blockContent} />
   </div>
 );
