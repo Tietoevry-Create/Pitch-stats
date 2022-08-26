130673c9e6768952dda7e0436a867a22ff9e1784
Ådne Forslund <adne.forslund@gmail.com>
2022-08-26 15:06:07 +0200

    adjust font for text

--- web/components/blockContent.jsx
+++ web/components/blockContent.jsx
@@ -1,7 +1,7 @@
 import { PortableText } from '@portabletext/react';
 
 const BlockContent = ({ blockContent, ...rest }) => (
-  <div className="container mx-auto px-4 md:px-40 py-10 text-lg">
+  <div className="container mx-auto px-4 md:px-40 py-10 text-xl">
     <PortableText value={blockContent} />
   </div>
 );
