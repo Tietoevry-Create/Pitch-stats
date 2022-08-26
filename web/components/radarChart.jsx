568608577cd052e48c7357dd9fc0b5fb8309bc9e
Ådne Forslund <adne.forslund@gmail.com>
2022-08-26 16:57:36 +0200

    add chart compo

--- web/components/radarChart.jsx
+++ web/components/radarChart.jsx
@@ -1,172 +1,56 @@
-import React, {Component} from 'react';
-import RadarChart from 'radar-chart';
-import {Hint} from 'index';
+import React, { PureComponent } from 'react';
+import { Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, ResponsiveContainer } from 'recharts';
 
-
-const DATA = [
+const data = [
   {
-    name: 'Spider5',
-    mileage: 5,
-    price: 5,
-    safety: 5,
-    performance: 5,
-    interior: 5,
-    warranty: 5,
-    fill: '#f8f8f8',
-    stroke: '#cccccc'
+    subject: 'SEO',
+    A: 55,
+    B: 60,
+    fullmark: 100,
   },
   {
-    name: 'Spider4',
-    mileage: 4,
-    price: 4,
-    safety: 4,
-    performance: 4,
-    interior: 4,
-    warranty: 4,
-    fill: 'white',
-    stroke: '#cccccc'
+    subject: 'Performance',
+    A: 98,
+    B: 130,
+    fullmark: 100,
   },
   {
-    name: 'Spider3',
-    mileage: 3,
-    price: 3,
-    safety: 3,
-    performance: 3,
-    interior: 3,
-    warranty: 3,
-    fill: '#f8f8f8',
-    stroke: '#cccccc'
+    subject: 'Accessibility',
+    A: 86,
+    B: 130,
+    fullmark: 100,
   },
   {
-    name: 'Spider2',
-    mileage: 2,
-    price: 2,
-    safety: 2,
-    performance: 2,
-    interior: 2,
-    warranty: 2,
-    fill: 'white',
-    stroke: '#cccccc'
+    subject: 'Best practices',
+    A: 99,
+    B: 100,
+    fullmark: 100,
   },
   {
-    name: 'Spider1',
-    mileage: 1,
-    price: 1,
-    safety: 1,
-    performance: 1,
-    interior: 1,
-    warranty: 1,
-    fill: '#f8f8f8',
-    stroke: '#cccccc'
+    subject: 'Something else',
+    A: 99,
+    B: 100,
+    fullmark: 100,
   },
-  {
-    name: 'Spider0',
-    mileage: 0.1,
-    price: 0.1,
-    safety: 0.1,
-    performance: 0.1,
-    interior: 0.1,
-    warranty: 0.1,
-    fill: '#f8f8f8',
-    stroke: '#cccccc'
-  },
-  {
-    name: 'Dagbladet',
-    mileage: 3,
-    price: 4,
-    safety: 5,
-    performance: 1.5,
-    interior: 4,
-    warranty: 4.5,
-    fill: 'rgba(114,172,240,0.5)',
-    stroke: 'rgba(114,172,240,0.2)'
-  }
+  
+  
+  
+  
 ];
 
-const tipStyle = {
-  display: 'flex',
-  color: '#fff',
-  background: '#000',
-  alignItems: 'center',
-  padding: '5px'
-};
-
-export default class RadarChartWithTooltips extends Component {
-  state = {
-    hoveredCell: false
-  };
+export default class CustomRadarChart extends PureComponent {
+  
 
   render() {
-    const {hoveredCell} = this.state;
-
     return (
-      <RadarChart
-        data={DATA}
-        tickFormat={t => {
-          return '';
-        }}
-        domains={[
-          {
-            name: 'mileage',
-            domain: [0, 5],
-            tickFormat: t => {
-              if (t < 5 && t > 0) {
-                return Math.round(t);
-              }
-              return '';
-            }
-          },
-          {
-            name: 'price',
-            domain: [0, 5],
-            getValue: d => d.price
-          },
-          {name: 'safety', domain: [0, 5], getValue: d => d.safety},
-          {name: 'performance', domain: [0, 5], getValue: d => d.performance},
-          {name: 'interior', domain: [0, 5], getValue: d => d.interior},
-          {name: 'warranty', domain: [0, 5], getValue: d => d.warranty}
-        ]}
-        width={300}
-        height={300}
-        onValueMouseOver={v => {
-          this.setState({hoveredCell: v});
-        }}
-        onValueMouseOut={v => this.setState({hoveredCell: false})}
-        style={{
-          polygons: {
-            strokeWidth: 1,
-            strokeOpacity: 0.8,
-            fillOpacity: 0.8
-          },
-          labels: {
-            textAnchor: 'middle'
-          },
-          axes: {
-            line: {
-              fillOpacity: 0.8,
-              strokeWidth: 0.5,
-              strokeOpacity: 0.8
-            },
-            ticks: {
-              fillOpacity: 0,
-              strokeOpacity: 0
-            },
-            text: {}
-          }
-        }}
-        colorRange={['transparent']}
-        hideInnerMostValues={false}
-        renderAxesOverPolygons={true}
-      >
-        {hoveredCell &&
-          hoveredCell.dataName === 'Dagbladet' && (
-            <Hint value={hoveredCell}>
-              <div style={tipStyle}>
-                {hoveredCell.domain}: {hoveredCell.value}
-              </div>
-            </Hint>
-          )}
-      </RadarChart>
+      <ResponsiveContainer width="100%" height="100%">
+        <RadarChart cx="50%" cy="50%" outerRadius="80%" data={data}>
+          <PolarGrid />
+          <PolarAngleAxis dataKey="subject" />
+          <PolarRadiusAxis />
+          <Radar dataKey="A" stroke="#560E62" fill="#8884d8" fillOpacity={0.5} />
+        </RadarChart>
+      </ResponsiveContainer>
     );
   }
 }
