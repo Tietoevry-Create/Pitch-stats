f2d49d4208e88418fc0a252c32196330b6722a71
torbjornhusebo <torbjorn.husebo@evry.com>
2022-08-26 11:32:40 +0200

    Removed ekstra padding on the sides as well as positioning header

--- web/components/footer.jsx
+++ web/components/footer.jsx
@@ -1,7 +1,7 @@
 import CustomLink from "./customLink";
 const Footer = () => (
   <footer className="bg-tepurple absolute bottom-0 w-full">
-  	<div className="mx-auto container grid grid-rows-5 grid-flow-col gap-4 py-10 px-10 md:px-52 text-pink-100">
+  	<div className="mx-auto container grid grid-rows-5 grid-flow-col gap-4 py-10 px-10 text-pink-100">
 			<CustomLink classes="px-3 text-white hover:underline" label={"Om dette prosjektet"}/>
 			<CustomLink classes="px-3 color-white hover:underline" label={"Kildekode"}/>
 			<CustomLink classes="px-3 color-white hover:underline" label={"Tietoevry"}/>
--- web/components/heading.jsx
+++ web/components/heading.jsx
@@ -1,19 +1,19 @@
 import PropTypes from 'prop-types';
 
 const Heading = ({ title, ...rest }) => (
-    <div className="bg-tepurple ">
-       <heading className="container mx-auto px-10 py-3 md:px-52">
+    <div className="flex bg-tepurple ">
+       <heading className="container mx-auto px-10 py-3">
         <div>
-        <h1 className="text-white font-bold">{title}</h1>
+          <h1 className="px-3 text-white font-bold">{title}</h1>
         </div>
        </heading>
     </div>
   );
 
-  
+
   Heading.propTypes = {
     title: PropTypes.string.isRequired  };
   export default Heading;
 
 
-  
+
