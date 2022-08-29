97f0ce049a64acfa1769a09b220be6bb4b4684f8
Ådne Forslund <adne.forslund@gmail.com>
2022-08-29 18:06:48 +0200

    make query for footer

--- web/util/queries.js
+++ web/util/queries.js
@@ -1,1 +1,4 @@
 export const blockContentQuery = 'blockContent[]{..., markDefs[]{ ...} }';
+
+export const footerQuery =
+  '{"footerData": *[_type == "footer" && _id == "footer"][0]{ footerLinks[]{_key, _type, label, linkReference->}}}';
