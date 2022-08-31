6e3153899ae10017af6fa82cfb848ef072a54460
Ådne Forslund <adne.forslund@gmail.com>
2022-08-31 09:57:47 +0200

    define title for footer and nav i Sanity

--- studio/deskStructure.js
+++ studio/deskStructure.js
@@ -9,10 +9,17 @@ export default () =>
         .child(S.document().schemaType("frontPage").documentId("frontPage")),
       S.listItem()
         .title("Footer")
-        .child(S.document().schemaType("footer").documentId("footer")),
+        .child(
+          S.document().schemaType("footer").documentId("footer").title("Footer")
+        ),
       S.listItem()
         .title("Menu")
-        .child(S.document().schemaType("navigation").documentId("navigation")),
+        .child(
+          S.document()
+            .schemaType("navigation")
+            .documentId("navigation")
+            .title("Menu")
+        ),
       S.listItem().title("Sites").child(S.documentTypeList("site")),
       ...S.documentTypeListItems().filter(
         (listItem) =>
