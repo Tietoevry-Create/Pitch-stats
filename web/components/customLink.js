5922df11e87e3fe1c5ab19028a35722c8074e676
Ådne Forslund <adne.forslund@gmail.com>
2022-08-31 09:27:14 +0200

    rename customLink

---
+++ web/components/customLink.js
@@ -0,0 +1,13 @@
+import PropTypes from 'prop-types';
+import Link from 'next/link';
+const CustomLink = ({ to, children, ...rest }) => (
+  <>
+    <Link href="/">{children}</Link>
+  </>
+);
+
+CustomLink.propTypes = {
+  to: PropTypes.string
+};
+
+export default CustomLink;
