a15f00f6838554d6a0914b22d108731aff10235a
Ådne Forslund <adne.forslund@gmail.com>
2022-09-01 16:34:55 +0200

    make footer more accesible

--- web/components/footer.js
+++ web/components/footer.js
@@ -1,7 +1,10 @@
 import CustomLink from './customLink';
 const Footer = ({ footerData: { footerLinks = [] } }) => {
   return (
-    <footer className="bg-tepurple bottom-0 w-full">
+    <footer aria-labelledby="footerHeading" className="bg-tepurple bottom-0 w-full">
+      <h2 id="footerHeading" className="sr-only">
+        Footer
+      </h2>
       <div className="mx-auto container grid grid-rows-5 grid-flow-col gap-4 py-10 px-4 md:px-20 text-pink-100">
         {footerLinks &&
           footerLinks.length > 0 &&
