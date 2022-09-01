ff475b831801b867de47c5f3d5d07b29298f8ee9
Ådne Forslund <adne.forslund@gmail.com>
2022-09-01 12:38:13 +0200

    add link to category and put in lede

--- web/components/category.js
+++ web/components/category.js
@@ -1,20 +1,22 @@
 import React from 'react';
-
+import CustomLink from './customLink';
 const Category = ({ children, ...rest }) => (
-  <div className="grid grid-cols-2">
-    <div>
-      <svg
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
-        className="fill-current text-black h-50 w-50 mr-2">
-        <path d="M3 10v11h6v-7h6v7h6v-11L12,3z" />
-      </svg>
-    </div>
-    <div className="container text-xl">
-      <h3>{children.title}</h3>
-      <span className="">{children.slug.current}</span>
-    </div>
-  </div>
+  <CustomLink>
+    <a className="grid grid-cols-2 ">
+      <div>
+        <svg
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+          className="fill-current text-black h-50 w-50 mr-2">
+          <path d="M3 10v11h6v-7h6v7h6v-11L12,3z" />
+        </svg>
+      </div>
+      <div className="container text-xl ">
+        <h3>{children.title}</h3>
+        <span className="">{children.lede}</span>
+      </div>
+    </a>
+  </CustomLink>
 );
 
 export default Category;
