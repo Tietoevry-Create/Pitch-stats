9d6667b2e9e3d23c209317653a0f1c424332e5f9
Ådne Forslund <adne.forslund@gmail.com>
2022-09-01 15:44:01 +0200

    add query for menu

--- web/util/queries.js
+++ web/util/queries.js
@@ -4,4 +4,7 @@ export const blockContentQuery = 'blockContent[]{..., markDefs[]{ ...} }';
 export const footerQuery =
   '"footerData": *[_type == "footer" && _id == "footer"][0]{ footerLinks[]{_key, _type, label, linkReference->}}';
 
+export const menuQuery =
+  '"menuData": *[_type == "navigation" && _id == "navigation"][0]{ menuLinks[]{_key, _type, label, linkReference->{...}}}';
+
 export const siteQuery = `*[_type == "site" && slug.current == $slug][0]{title, webSiteUrl, ${blockContentQuery},  ...}`;
