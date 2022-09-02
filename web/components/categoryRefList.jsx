23e02db4b8080c0e08331ba614cc52fa3c7eaa0a
Ådne Forslund <adne.forslund@gmail.com>
2022-09-02 13:28:05 +0200

    make h2 smaller on mobile

--- web/components/categoryRefList.jsx
+++ web/components/categoryRefList.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const CategoryRefList = ({ categoryList, ...rest }) => (
   <section className="container mx-auto px-4 md:px-24 py-10">
-    <h2 className="font-bold pb-1 border-b-4 border-black">Kategorier</h2>
+    <h2 className="text-3xl md:text-4xl font-bold pb-1 border-b-4 border-black">Kategorier</h2>
     <div className="grid grid-cols-1 md:grid-cols-2 gap-20 my-14">
       {categoryList &&
         categoryList.length > 0 &&
