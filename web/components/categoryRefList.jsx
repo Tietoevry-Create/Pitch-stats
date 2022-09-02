601e5084c680846f6ab40ddf41b71f4b871ae856
Ådne Forslund <adne.forslund@gmail.com>
2022-09-02 12:36:56 +0200

    add border under h2s

--- web/components/categoryRefList.jsx
+++ web/components/categoryRefList.jsx
@@ -2,14 +2,14 @@ import Category from 'components/category';
 import React from 'react';
 
 const CategoryRefList = ({ categoryList, ...rest }) => (
-  <div className="container mx-auto px-4 md:px-24 py-10">
-    <h2 className="font-bold">Kategorier</h2>
+  <section className="container mx-auto px-4 md:px-24 py-10">
+    <h2 className="font-bold border-b-4 border-black">Kategorier</h2>
     <div className="grid grid-cols-1 md:grid-cols-2 gap-20 my-14">
       {categoryList &&
         categoryList.length > 0 &&
         categoryList.map((category) => <Category key={category._id}>{category}</Category>)}
     </div>
-  </div>
+  </section>
 );
 
 export default CategoryRefList;
