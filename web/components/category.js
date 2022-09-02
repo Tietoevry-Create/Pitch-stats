398e1e58d1ac1e70312d086341f1b6aef4366ba6
Ådne Forslund <adne.forslund@gmail.com>
2022-09-02 13:34:17 +0200

    smaller text on mobile

--- web/components/category.js
+++ web/components/category.js
@@ -6,13 +6,12 @@ const Category = ({ children, ...rest }) => (
       <svg
         viewBox="0 0 24 24"
         xmlns="http://www.w3.org/2000/svg"
-        className="fill-current text-black h-46 w-46 mr-2"
-      >
+        className="fill-current text-black h-46 w-46 mr-2">
         <path d={children.path} />
       </svg>
       <div className="grid grid-row-2 gap-7 py-3">
         <h3 className="text-2xl group-hover:underline">{children.title}</h3>
-        <p className="text-xl">{children.lede}</p>
+        <p className="text-lg md:text-xl">{children.lede}</p>
       </div>
     </a>
   </CustomLink>
