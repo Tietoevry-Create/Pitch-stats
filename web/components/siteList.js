49856181274e96e66ad66bc068a4586755495300
Cecilie Vågenes <cecilie.gjerde.vagenes@tietoevry.com>
2022-09-05 12:32:54 +0200

    fix

--- web/components/siteList.js
+++ web/components/siteList.js
@@ -6,31 +6,33 @@ const SiteList = ({ siteList, ...rest }) => (
     <section>
       <h2>Sider</h2>
       <ul>
-        <li className="grid grid-cols-6 p-2 rounded font-semibold">
-          <span>Navn</span>
-          <span>Kategori</span>
-          <span>Utslipp</span>
-          <span>Acc</span>
-          <span>Perf</span>
-          <span>SEO</span>
+        <li className="grid md:grid-desktop grid-mobile p-2 rounded font-semibold md:gap-4 gap-2">
+            <span className='overflow-hidden text-ellipsis'>Nr</span>
+          <span className='overflow-hidden text-ellipsis'>Navn</span>
+          <span className='overflow-hidden text-ellipsis'>Kategori</span>
+          <span className='overflow-hidden text-ellipsis'>Utslipp</span>
+          <span className='invisible md:visible'>Acc</span>
+          <span className='invisible md:visible'>Perf</span>
+          <span className='invisible md:visible'>SEO</span>
         </li>
       </ul>
       <ul>
         {siteList &&
           siteList.length > 0 &&
-          siteList.map((site) => (
+          siteList.map((site, index) => (
             <li
               key={site._id}
               className="p-2 border-solid border-tepurple border-2 rounded hover:bg-tepurple/20 border-t-0 first:border-t-2"
             >
               <CustomLink>
-                <a className="grid grid-cols-6">
-                  <span>{site.title}</span>
-                  <span>{site.category.title}</span>
-                  <span>0.39 CO₂</span>
-                  <span>90%</span>
-                  <span>60%</span>
-                  <span>80%</span>
+                <a className="grid md:grid-desktop grid-mobile md:gap-4 gap-2 whitespace-nowrap">
+                    <span className=' w-5 justify-self-center'>{index + 1}.</span>
+                    <span className=' overflow-hidden text-ellipsis'>{site.title}</span>
+                    <span className=' overflow-hidden text-ellipsis'>{site.category.title}</span>
+                    <span className=' overflow-hidden text-ellipsis'>0.39 CO₂</span>
+                    <span className=' overflow-hidden text-ellipsis invisible md:visible'>90%</span>
+                    <span className=' overflow-hidden text-ellipsis invisible md:visible'>60%</span>
+                    <span className=' overflow-hidden text-ellipsis invisible md:visible'>80%</span>
                 </a>
               </CustomLink>
             </li>
