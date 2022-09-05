3ffa17f6fe09572c77c0e90b9c3631896854976d
Ådne Forslund <adne.forslund@gmail.com>
2022-09-05 16:42:15 +0200

    remove redundant css class

--- web/components/navbar.js
+++ web/components/navbar.js
@@ -34,7 +34,7 @@ const Navbar = ({ menuData = {} }) => {
             menuLinks.length > 0 &&
             menuLinks.map((menuLink) => (
               <CustomLink key={menuLink._key}>
-                <a className="text-lg lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-black font-bold items-center justify-center rounded-lg border-2 border-yellow-50 hover:border-tepurple hover:bg-tepurple/20">
+                <a className="text-lg lg:inline-flex lg:w-auto w-full px-3 py-2 text-black font-bold items-center justify-center rounded-lg border-2 border-yellow-50 hover:border-tepurple hover:bg-tepurple/20">
                   {menuLink.label}
                 </a>
               </CustomLink>
