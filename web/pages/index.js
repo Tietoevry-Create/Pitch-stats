f1fddddf8cf437e2c94e7f618394ba5542a6578d
torbjornhusebo <torbjorn.husebo@evry.com>
2022-09-08 13:20:59 +0200

    New animated feature if we want to use it

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head';
-
+import {useEffect, useRef, useState} from 'react'
 import client from 'util/client.js';
 import Layout from 'components/layout';
 import Heading from 'components/heading';
 import BlockContent from 'components/blockContent';
 import SiteList from 'components/siteList';
 import CategoryRefList from '/components/categoryRefList';
+
 import { footerQuery, blockContentQuery, menuQuery } from 'util/queries';
 export default function Home({
   menuData = {},
@@ -16,7 +17,42 @@ export default function Home({
 }) {
   const { title, categoryList, blockContent } = pageData;
 
-  return (
+	const blockContentRef = useRef();
+	const [blockContentVisibility, setBlockContentVisibility] = useState();
+
+	const categoryRefListRef = useRef();
+	const [categoryRefListVisibility, setCategoryRefListVisibility] = useState();
+
+	const siteListRef = useRef();
+  const [siteListVisibility, setSiteListVisibility] = useState();
+
+	useEffect(() => {
+		const blockContentObserver = new IntersectionObserver((entries) => {
+			const entry = entries[0];
+			setBlockContentVisibility(entry.isIntersecting);
+		});
+
+		const categoryRefListObserver = new IntersectionObserver((entries) => {
+			const entry = entries[0];
+			setCategoryRefListVisibility(entry.isIntersecting);
+		});
+
+		const siteListObserver = new IntersectionObserver((entries) => {
+			const entry = entries[0];
+			setSiteListVisibility(entry.isIntersecting);
+		});
+
+		blockContentObserver.observe(blockContentRef.current);
+		categoryRefListObserver.observe(categoryRefListRef.current);
+		siteListObserver.observe(siteListRef.current);
+		return () => {
+			siteListObserver.disconnect();
+			categoryRefListObserver.disconnect();
+			siteListObserver.disconnect();
+		}
+	}, []);
+
+	return (
     <div>
       <Head>
         <title>Statistikk</title>
@@ -25,11 +61,15 @@ export default function Home({
       </Head>
       <Layout footerData={footerData} menuData={menuData}>
         <Heading title={title} />
-        <div className="container mx-auto px-4 md:px-24 py-4 md:py-10 text-xl">
-          <BlockContent blockContent={blockContent || []} />
+        <div className={`${blockContentVisibility ? 'animate-fadeIn' : ''}`}>
+          <BlockContent refs={blockContentRef} blockContent={blockContent || []} />
         </div>
-        <CategoryRefList categoryList={categoryList || []} />
-        <SiteList siteList={siteList || []} />
+				<div className={`${categoryRefListVisibility ? 'animate-fadeIn' : ''}`}>
+					<CategoryRefList refs={categoryRefListRef} categoryList={categoryList || []} />
+				</div>
+				<div className={`${siteListVisibility ? 'animate-fadeIn' : ''}`}>
+        	<SiteList refs={siteListRef} siteList={siteList || []} />
+				</div>
       </Layout>
     </div>
   );
