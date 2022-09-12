706e575931a6c99cf90ce84568fc92d9a8d11195
torbjornhusebo <torbjorn.husebo@evry.com>
2022-09-12 13:29:59 +0200

    Better view

--- web/components/category.js
+++ web/components/category.js
@@ -2,19 +2,24 @@ import React from 'react';
 import CustomLink from './customLink';
 const Category = ({ category, ...rest }) => (
   <CustomLink to={category}>
-    <a className="items-start grid grid-cols-2 gap-3 py-6 px-3 rounded-lg border-2 border-yellow-50 hover:border-tepurple hover:bg-tepurple/20 hover:scale-110 group">
-      <svg
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
-        className="fill-current text-black h-46 w-46 mr-2"
-      >
-        <path d={category.path} />
-      </svg>
+    <a className="pb-6 pt-16 px-3 rounded-lg border-2 border-yellow-50 hover:border-tepurple hover:bg-tepurple/20 hover:scale-110 group">
+      <div className="items-center md:items-start grid grid-cols-2 gap-3">
+        <svg
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+          className="fill-current text-black h-46 w-46 mr-2"
+        >
+          <path d={category.path} />
+        </svg>
 
-      <div className="grid grid-row-2 gap-7">
-        <h3 className="text-2xl group-hover:underline">{category.title}</h3>
-        <p className="text-lg md:text-xl">{category.lede}</p>
+        <div className="grid grid-row-2 gap-7">
+          <h3 className="text-2xl group-hover:underline">{category.title}</h3>
+          <p className="hidden md:block text-lg md:text-xl ">{category.lede}</p>
+        </div>
       </div>
+      <p className="md:hidden text-lg md:text-xl rounded-lg mb-auto border-2 border-tepurple py-5 px-5 md:py-10 md:px-16">
+        {category.lede}
+      </p>
     </a>
   </CustomLink>
 );
