aea607d92f3cc43f7a970765c5ba7b490a0650c9
Ådne Forslund <adne.forslund@gmail.com>
2022-09-19 12:27:17 +0200

    start metadata

--- web/components/metaData.js
+++ web/components/metaData.js
@@ -1,3 +1,11 @@
-const MetaData = ({ ...rest }) => <></>;
+import { NextSeo } from 'next-seo';
+const MetaData = ({ document, ...rest }) => {
+  const { title } = document;
+  return (
+    <>
+      <NextSeo title={title} />
+    </>
+  );
+};
 
 export default MetaData;
