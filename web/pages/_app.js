336647f90cc65de529b3675977b90491a5a86c35
Emil Flatøy Hodneland <emil.flatoyhodneland@tietoevry.com>
2022-09-21 12:56:11 +0200

    Changed so that prettier fixes formatting on commit, instead of blocking the commit.

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -5,7 +5,7 @@ function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
 if (typeof window !== 'undefined' && process.env.NODE_ENV !== 'production') {
-  const ReactDOM = require('react-dom');
+  const ReactDOM = require(' react-dom');
   const axe = require('@axe-core/react');
   axe(React, ReactDOM, 1000);
 }
