fbad478300bcca5feca0eda8a8903e8f45f20c8d
Martin Bruland <martinbruland@icloud.com>
2022-09-26 10:03:00 +0200

    prettier

--- web/components/icons/Icon404.js
+++ web/components/icons/Icon404.js
@@ -1,6 +1,2 @@
-const Icon404 = () => (
-    <svg class="h-96">
-    </svg>
-);  
+const Icon404 = () => <svg class="h-96"></svg>;
 export default Icon404;
-  
--- web/pages/404.js
+++ web/pages/404.js
@@ -3,36 +3,20 @@ import { Layout } from 'components/Layout';
 import { Heading } from 'components/Heading';
 import { footerQuery, blockContentQuery, menuQuery } from 'util/queries';
 import { MetaData } from 'components/MetaData';
-import Icon404 from 'components/icons/Icon404'
+import Icon404 from 'components/icons/Icon404';
 
-
-
-
-export default function Custom404({
-    menuData = {},
-    pageData = {},
-    footerData = {},
-  }) {
-    
+export default function Custom404({ menuData = {}, pageData = {}, footerData = {} }) {
   return (
-      <>
-        <MetaData document={pageData} />
+    <>
+      <MetaData document={pageData} />
 
-        <Layout footerData={footerData} menuData={menuData}>
-            
-            <Heading title={'Page not found'} />
-
-            <Icon404 />
-
-        </Layout>
-      </>
-  )
+      <Layout footerData={footerData} menuData={menuData}>
+        <Heading title={'Page not found'} />
+        <Icon404 />
+      </Layout>
+    </>
+  );
 }
-
-
-
-
-
 
 export async function getStaticProps() {
   const query = `{"siteList": *[_type=="site"]{title, slug, _type, _id, "category": categoryReference->{title}}, "pageData": *[_type == 'frontPage' && _id == 'frontPage' && !(_id in path("drafts.**"))][0]{title, ${blockContentQuery}, "categoryList": categoryRefList[]->{_id, _type, title, slug, lede, path, ... }, ...}, ${footerQuery}, ${menuQuery} }`;
@@ -41,7 +25,7 @@ export async function getStaticProps() {
   return {
     props: {
       footerData: footerData,
-      menuData: menuData,
+      menuData: menuData
     },
     revalidate: 50
   };
