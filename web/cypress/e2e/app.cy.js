925eb4a02b21a37cce5c5eb71b235bb24a222254
Martin Bruland <martinbruland@icloud.com>
2022-09-26 12:48:58 +0200

    Code CleanUp: "Navigation test"

--- web/cypress/e2e/app.cy.js
+++ web/cypress/e2e/app.cy.js
@@ -1,71 +1,17 @@
 describe('Page Navigation from "Landingpage". ', () => {
-  // HEADER
+  const baseURL = 'http://localhost:3000';
+  const headerLinkNames = ['Funksjoner', 'Om', 'Tietoevry', 'Sammenligning']; // , 'Sider' === cross-origin.
+  const mainLinkNames = ['Nyheter', 'Offentlig', 'Teknologi', 'Strømselskap'];
+  const footerLinkNames = ['Tietoevry', 'Tietoevry', 'KIldekode']; // , 'Om dette prosjektet' === cross-origin.
 
-  it('Navigate to "Funksjoner". ', () => {
-    cy.visit('http://localhost:3000');
-    cy.contains('Funksjoner').click();
-  });
+  const allLinks = [headerLinkNames, mainLinkNames, footerLinkNames];
 
-  /*  CROSS-ORIGIN -> GOOGLE.COM
-  it('Navigate to "Sider". ', () => {
-    cy.visit('http://localhost:3000')
-    cy.contains('Sider').click()
-  })
-  */
-
-  it('Navigate to "Om". ', () => {
-    cy.visit('http://localhost:3000');
-    cy.contains('Om').click();
-  });
-
-  it('Navigate to "TietoEvry". ', () => {
-    cy.visit('http://localhost:3000');
-    cy.contains('TietoEvry').click();
-  });
-
-  it('Navigate to "Sammenligning". ', () => {
-    cy.visit('http://localhost:3000');
-    cy.contains('Sammenligning').click();
-  });
-
-  // MAIN CONTENT
-
-  it('Navigate to "Category/Nyheter". ', () => {
-    cy.visit('http://localhost:3000');
-    cy.contains('Nyheter').click();
-  });
-
-  it('Navigate to "Category/Offentlig". ', () => {
-    cy.visit('http://localhost:3000');
-    cy.contains('Offentlig').click();
-  });
-
-  it('Navigate to "Category/Teknologi". ', () => {
-    cy.visit('http://localhost:3000');
-    cy.contains('Teknologi').click();
-  });
-
-  it('Navigate to "Category/Strømselskap". ', () => {
-    cy.visit('http://localhost:3000');
-    cy.contains('Strømselskap').click();
-  });
-
-  // FOOTER
-
-  /*  CROSS-ORIGIN -> GOOGLE.COM
-  it('Navigate to "Om dette prosjektet". ', () => {
-    cy.visit('http://localhost:3000')
-    cy.contains('Om dette prosjektet').click()
-  })
-  */
-
-  it('Navigate to "Tietoevry". ', () => {
-    cy.visit('http://localhost:3000');
-    cy.contains('Tietoevry').click();
-  });
-
-  it('Navigate to "Kildekode". ', () => {
-    cy.visit('http://localhost:3000');
-    cy.contains('KIldekode').click();
+  allLinks.forEach((group) => {
+    group.forEach((subPage) => {
+      it(`Navigate to ${subPage}`, () => {
+        cy.visit(baseURL);
+        cy.contains(subPage).click();
+      });
+    });
   });
 });
