0bd7e6ebec5b01e6c5d8684638f89b3cc5e8e37a
Martin Bruland <martinbruland@icloud.com>
2022-09-26 12:26:58 +0200

    Added a test for checking page navigation.

--- web/cypress/e2e/app.cy.js
+++ web/cypress/e2e/app.cy.js
@@ -1,6 +1,80 @@
 
-describe('Page Navigation', () => {
-  it('passes', () => {
+describe('Page Navigation from "Landingpage". ', () => {
+
+  // HEADER
+
+  it('Navigate to "Funksjoner". ', () => {
     cy.visit('http://localhost:3000')
+    cy.contains('Funksjoner').click()
+  });
+
+  /*  CROSS-ORIGIN -> GOOGLE.COM
+  it('Navigate to "Sider". ', () => {
+    cy.visit('http://localhost:3000')
+    cy.contains('Sider').click()
   })
+  */
+
+  it('Navigate to "Om". ', () => {
+    cy.visit('http://localhost:3000')
+    cy.contains('Om').click()
+  })
+
+  it('Navigate to "TietoEvry". ', () => {
+    cy.visit('http://localhost:3000')
+    cy.contains('TietoEvry').click()
+  })
+
+  it('Navigate to "Sammenligning". ', () => {
+    cy.visit('http://localhost:3000')
+    cy.contains('Sammenligning').click()
+  })
+
+
+  // MAIN CONTENT
+
+  it('Navigate to "Category/Nyheter". ', () => {
+    cy.visit('http://localhost:3000')
+    cy.contains('Nyheter').click()
+  })
+
+  it('Navigate to "Category/Offentlig". ', () => {
+    cy.visit('http://localhost:3000')
+    cy.contains('Offentlig').click()
+  })
+
+  it('Navigate to "Category/Teknologi". ', () => {
+    cy.visit('http://localhost:3000')
+    cy.contains('Teknologi').click()
+  })
+
+  it('Navigate to "Category/Strømselskap". ', () => {
+    cy.visit('http://localhost:3000')
+    cy.contains('Strømselskap').click()
+  })
+
+
+  // FOOTER
+
+  /*  CROSS-ORIGIN -> GOOGLE.COM
+  it('Navigate to "Om dette prosjektet". ', () => {
+    cy.visit('http://localhost:3000')
+    cy.contains('Om dette prosjektet').click()
+  })
+  */
+
+  it('Navigate to "Tietoevry". ', () => {
+    cy.visit('http://localhost:3000')
+    cy.contains('Tietoevry').click()
+  })
+
+  it('Navigate to "Kildekode". ', () => {
+    cy.visit('http://localhost:3000')
+    cy.contains('KIldekode').click()
+  })
+
+  
+
+  
+
 })
