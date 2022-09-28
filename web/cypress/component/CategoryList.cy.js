ce2011490891c707fbb7b2aa1fd64b3a6c9f57ec
Martin Bruland <martinbruland@icloud.com>
2022-09-28 11:37:54 +0200

    added component testing for components: CategoryList, Counter, CustomLink

---
+++ web/cypress/component/CategoryList.cy.js
@@ -0,0 +1,68 @@
+import { Counter } from '../../components/Counter';
+
+const localData = {
+  categoryList: [
+    {
+      _createdAt: '2022-08-08T14:13:05Z',
+      _id: '9908a364-4a79-46d2-976c-a175404f4943',
+      _rev: 'fT2DAxGuAIAZVP80ZJWfQH',
+      _type: 'category',
+      _updatedAt: '2022-09-06T10:24:38Z',
+      lede: 'Nyheter er ny informasjon om hendelser av interesse for publikum, formidlet av en journalist gjennom ulike medier som for eksempel en nettside. Her en liste av de mest leste i Norge.',
+      path: 'M8.627,7.885C8.499,8.388,7.873,8.101,8.13,8.177L4.12,7.143c-0.218-0.057-0.351-0.28-0.293-0.498c0.057-0.218,0.279-0.351,0.497-0.294l4.011,1.037C8.552,7.444,8.685,7.667,8.627,7.885 M8.334,10.123L4.323,9.086C4.105,9.031,3.883,9.162,3.826,9.38C3.769,9.598,3.901,9.82,4.12,9.877l4.01,1.037c-0.262-0.062,0.373,0.192,0.497-0.294C8.685,10.401,8.552,10.18,8.334,10.123 M7.131,12.507L4.323,11.78c-0.218-0.057-0.44,0.076-0.497,0.295c-0.057,0.218,0.075,0.439,0.293,0.495l2.809,0.726c-0.265-0.062,0.37,0.193,0.495...',
+      slug: {
+        _type: 'slug',
+        current: 'nyheter'
+      },
+      title: 'Nyheter'
+    },
+    {
+      _createdAt: '2022-08-08T14:13:15Z',
+      _id: '997f7bc5-c790-417f-b736-a8f0c2c7d6b1',
+      _rev: 'D7m4uX1OTWXTDbfqfbhAcR',
+      _type: 'category',
+      _updatedAt: '2022-09-13T11:48:01Z',
+      lede: 'Nettsider som driftes av statlige, regionale eller lokale myndigheter, offentligrettslige organer og sammenslutninger dannet av én eller flere slike myndigheter eller offentligrettslige organer. ',
+      path: 'M17.283,5.549h-5.26V4.335c0-0.222-0.183-0.404-0.404-0.404H8.381c-0.222,0-0.404,0.182-0.404,0.404v1.214h-5.26c-0.223,0-0.405,0.182-0.405,0.405v9.71c0,0.223,0.182,0.405,0.405,0.405h14.566c0.223,0,0.404-0.183,0.404-0.405v-9.71C17.688,5.731,17.506,5.549,17.283,5.549 M8.786,4.74h2.428v0.809H8.786V4.74z M16.879,15.26H3.122v-4.046h5.665v1.201c0,0.223,0.182,0.404,0.405,0.404h1.618c0.222,0,0.405-0.182,0.405-0.404v-1.201h5.665V15.26z M9.595,9.583h0.81v2.428h-0.81V9.583zM16.879,10.405h-5.665V9.19c0-0.222-0...',
+      slug: {
+        _type: 'slug',
+        current: 'offentlig'
+      },
+      title: 'Offentlig'
+    },
+    {
+      _createdAt: '2022-08-31T10:38:12Z',
+      _id: '0f6d1ab6-4eab-4596-a73e-a74a6ae127ba',
+      _rev: 'fT2DAxGuAIAZVP80ZJWaWf',
+      _type: 'category',
+      _updatedAt: '2022-09-06T10:24:10Z',
+      lede: 'Det finnes en rekke norske nettsider som spesialiserer seg på å informere befolkningen om ny og spennende teknologi som jobbes med. Her er en liste av de mest populære i Norge.',
+      path: 'M17.237,3.056H2.93c-0.694,0-1.263,0.568-1.263,1.263v8.837c0,0.694,0.568,1.263,1.263,1.263h4.629v0.879c-0.015,0.086-0.183,0.306-0.273,0.423c-0.223,0.293-0.455,0.592-0.293,0.92c0.07,0.139,0.226,0.303,0.577,0.303h4.819c0.208,0,0.696,0,0.862-0.379c0.162-0.37-0.124-0.682-0.374-0.955c-0.089-0.097-0.231-0.252-0.268-0.328v-0.862h4.629c0.694,0,1.263-0.568,1.263-1.263V4.319C18.5,3.625,17.932,3.056,17.237,3.056 M8.053,16.102C8.232,15.862,8.4,15.597,8.4,15.309v-0.89h3.366v0.89c0,0.303,0.211,0.562,0.419,0.79...',
+      slug: {
+        _type: 'slug',
+        current: 'teknologi'
+      },
+      title: 'Teknologi'
+    },
+    {
+      _createdAt: '2022-08-31T10:37:39Z',
+      _id: '79e88586-f3e8-43c9-ae48-bfc786ea0488',
+      _rev: 'fT2DAxGuAIAZVP80ZJVFbg',
+      _type: 'category',
+      _updatedAt: '2022-09-06T10:17:10Z',
+      lede: 'Strømleverandørene kjøper strøm på den nordiske strømbørsen, og selger denne strømmen videre til deg som kunde. Her er en liste over nettsidene til noen av de største aktørene i Norge.',
+      path: 'M18.796,6.974h-0.652V5.201c0-0.457-0.372-0.831-0.831-0.831H1.204c-0.459,0-0.831,0.373-0.831,0.831v9.599c0,0.459,0.372,0.831,0.831,0.831h16.11c0.459,0,0.831-0.372,0.831-0.831v-1.704h0.652c0.459,0,0.831-0.37,0.831-0.831v-4.46C19.627,7.346,19.256,6.974,18.796,6.974z M16.483,13.969H2.034V6.031h14.448v1.773v4.46V13.969z',
+      slug: {
+        _type: 'slug',
+        current: 'strømselskap'
+      },
+      title: 'Strømselskap'
+    }
+  ]
+};
+
+describe('<Counter />', () => {
+  it('Check mount', () => {
+    cy.mount(<Counter />);
+  });
+});
---
+++ web/cypress/component/CustomLink.cy.js
@@ -0,0 +1,23 @@
+import { CustomLink } from '../../components/CustomLink';
+
+const localData = {
+  to: {
+    _id: '0a138ebe-626a-4978-9783-ff6745ce1852',
+    _type: 'site',
+    category: {
+      title: 'Strømselskap'
+    },
+    slug: {
+      _type: 'slug',
+      current: 'talkmore'
+    },
+    title: 'Talkmore'
+  },
+  children: '<a />'
+};
+
+describe('<CustomLink />', () => {
+  it('Check mount', () => {
+    cy.mount(<CustomLink to={localData} children={<a />} />);
+  });
+});
