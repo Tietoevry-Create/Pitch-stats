2a98d73ab1d4155031b0a2d0ecfb7163b5c1838f
Ådne Forslund <adne.forslund@gmail.com>
2022-09-28 11:05:54 +0200

    remove redundant markup

--- web/pages/site/[slug].js
+++ web/pages/site/[slug].js
@@ -13,7 +13,7 @@ export default function Site({ pageData = {}, footerData = {}, menuData, preview
   const { title, webSiteUrl = '', blockContent = [] } = pageData;
 
   return (
-    <div>
+    <>
       <MetaData document={pageData} />
 
       <Layout footerData={footerData} menuData={menuData}>
@@ -47,7 +47,7 @@ export default function Site({ pageData = {}, footerData = {}, menuData, preview
           </div>
         </div>
       </Layout>
-    </div>
+    </>
   );
 }
 
@@ -71,6 +71,6 @@ export async function getStaticPaths() {
 
   return {
     paths: paths.map((slug) => ({ params: { slug } })),
-    fallback: false
+    fallback: true
   };
 }
