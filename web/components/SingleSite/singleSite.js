93599b92385b45b8d57a6bf52a88b4105da989e0
Ådne Forslund <adne.forslund@gmail.com>
2022-09-29 10:36:37 +0200

    go from hidden to dislay none

--- web/components/SingleSite/singleSite.js
+++ web/components/SingleSite/singleSite.js
@@ -17,20 +17,16 @@ const SingleSite = ({ site, index, ...rest }) => {
   }, []);
 
   return (
-    <li className="p-2 border-solid border-heroblue border-2 hover:bg-peach10 border-t-0 first:border-t-2">
+    <li className="p-2 border-solid border-tepurple border-2 rounded hover:bg-tepurple/20 border-t-0 first:border-t-2">
       <CustomLink to={site}>
         <a className="grid md:grid-desktop grid-mobile md:gap-4 gap-2 whitespace-nowrap">
           <span className=" w-5 justify-self-center">{index + 1}.</span>
           <span className=" overflow-hidden text-ellipsis">{site.title}</span>
           <span className=" overflow-hidden text-ellipsis">{site.category.title}</span>
           <span className=" overflow-hidden text-ellipsis">0.{carbon} CO₂</span>
-          <span className=" overflow-hidden text-ellipsis invisible md:visible">
-            {accesibility}%
-          </span>
-          <span className=" overflow-hidden text-ellipsis invisible md:visible">
-            {performance}%
-          </span>
-          <span className=" overflow-hidden text-ellipsis invisible md:visible">{seo}%</span>
+          <span className=" overflow-hidden text-ellipsis hidden md:block">{accesibility}%</span>
+          <span className=" overflow-hidden text-ellipsis hidden md:block">{performance}%</span>
+          <span className=" overflow-hidden text-ellipsis hidden md:block">{seo}%</span>
         </a>
       </CustomLink>
     </li>
