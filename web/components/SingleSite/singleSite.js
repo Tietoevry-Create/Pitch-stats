ce1410aaf37dd2d792d8cf2899f5edf496b2d503
Martin Bruland <martinbruland@icloud.com>
2022-10-06 13:53:11 +0200

    added minor styling changes

--- web/components/SingleSite/singleSite.js
+++ web/components/SingleSite/singleSite.js
@@ -18,24 +18,30 @@ const SingleSite = ({ site, index, ...rest }) => {
 
   return (
     <tr>
-      {[index + 1, site.title, site.category.title, carbon, accessibility, performance, seo].map(
-        (val, keyIndex) => (
-          <td id={keyIndex} className="text-center p-0 pt-2">
-            <div
-              className={
-                (keyIndex === 0
-                  ? 'rounded-l border-t border-b border-l '
-                  : 'rounded-none border-t border-b ') +
-                'border-stone-900 h-10 flex items-center justify-center'
-              }>
-              {val}
-            </div>
-          </td>
-        )
-      )}
+      {[
+        index + 1 + '.',
+        site.title,
+        site.category.title,
+        carbon,
+        accessibility,
+        performance,
+        seo
+      ].map((val, keyIndex) => (
+        <td id={keyIndex} className="text-center p-0 pt-2">
+          <div
+            className={
+              (keyIndex === 0
+                ? 'rounded-l border-t-2 border-b-2 border-l-2 '
+                : 'rounded-none border-t-2 border-b-2 ') +
+              'border-violet h-10 flex items-center justify-center'
+            }>
+            {val}
+          </div>
+        </td>
+      ))}
 
       <td className="text-center p-0 pt-2">
-        <div className="rounded-r border-t border-b border-r border-stone-900 h-10 flex items-center justify-center">
+        <div className="rounded-r border-t-2 border-b-2 border-r-2 border-violet h-10 flex items-center justify-center">
           <CustomLink to={site}>
             <a className="rounded-full px-2 text-base bg-peach hover:bg-peach40 border-transparent border-2 hover:border-heroblue hover:border-solid">
               {'Se mer'}
