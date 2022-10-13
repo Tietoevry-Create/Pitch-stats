99a37d2cb343c86a4d1f79ae3f41760b70ae5c6d
Martin Bruland <martin.bruland@tietoevry.com>
2022-10-13 13:00:47 +0200

    Update web/.storybook/main.js
    Co-authored-by: Sindre Bøyum <boyum@users.noreply.github.com>

--- web/.storybook/main.js
+++ web/.storybook/main.js
@@ -2,7 +2,7 @@ module.exports = {
   stories: [
     '../stories/**/*.stories.mdx',
     '../stories/**/*.stories.@(js|jsx|ts|tsx)',
-    '../components/*/*.stories.js'
+    '../**/*.stories.@(js|jsx|ts|tsx)'
   ],
   addons: [
     '@storybook/addon-links',
