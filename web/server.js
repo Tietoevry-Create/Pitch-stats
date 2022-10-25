0b9d098a7bfe418c8e46cf88afa7a7ebb3b6daee
torbjornhusebo <torbjorn.husebo@evry.com>
2022-10-25 10:01:23 +0200

    Testing with zip

--- web/server.js
+++ web/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const next = require('next');
+const process = require('process');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
