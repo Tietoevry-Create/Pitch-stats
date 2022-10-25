5a7be2e14d8575d0740a168dc4027104d16eb196
Vegard Valvik <vegard.valvik@gmail.com>
2022-10-25 13:27:55 +0200

    Try different server.js

--- web/server.js
+++ web/server.js
@@ -1,29 +1,11 @@
 const express = require('express');
-const next = require('next');
-const process = require('process');
-
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
-
-// Your app will get the Azure port from the process.enc.PORT
+const app = express();
 const port = process.env.PORT || 3000;
 
-app
-  .prepare()
-  .then(() => {
-    const server = express();
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
-    server.get('*', (req, res) => {
-      return handle(req, res);
-    });
-
-    server.listen(port, (err) => {
-      if (err) throw err;
-      console.log('> Ready on http://localhost:3000');
-    });
-  })
-  .catch((ex) => {
-    console.error(ex.stack);
-    process.exit(1);
-  });
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
