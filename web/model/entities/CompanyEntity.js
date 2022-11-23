5758de0dd23a72620fbb69af6ec91c1e808e813d
Martin Bruland <martinbruland@icloud.com>
2022-11-23 15:34:24 +0100

    Added new entitiy: Company

---
+++ web/model/entities/CompanyEntity.js
@@ -0,0 +1,30 @@
+export default class CompanyEntity {
+  constructor() {
+    title = '';
+    orgNr = '';
+    dateFounded = '';
+
+    sectorTitle = '';
+    sectorCode = '';
+    industryTitle = '';
+    industryCode = '';
+
+    activeCEO = '';
+    numberOfEmployees = '';
+
+    address = '';
+    postalCode = '';
+    municipality = '';
+    state = '';
+    country = '';
+
+    // LIGHTHOUSE ANALYSIS AND C02 EMISSION
+    website = '';
+    websitePerformanceScore = '';
+    websiteAccessibilityScore = '';
+    websiteBestPracticesScore = '';
+    websiteSeoScore = '';
+    websitePwaScore = '';
+    websiteCO2Score = '';
+  }
+}
