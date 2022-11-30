618345b11a1abe8a5b36d7d67d85a2bc5c4fe10f
Martin Bruland <martinbruland@icloud.com>
2022-11-30 09:04:12 +0100

    Added boundaries to map, based on postal addr

--- web/pages/utforsker/index.js
+++ web/pages/utforsker/index.js
@@ -1,31 +1,108 @@
 import { useEffect, useState, useRef } from 'react';
 import Link from 'next/link';
-import mapboxgl from 'mapbox-gl';
-
+import maplibregl, { Map } from 'maplibre-gl';
 import { Sidebar } from 'components/Sidebar';
 
 export default function Home({}) {
-  const dataFetchedRef = useRef(false);
+  var mapData = require('../../model/mapData.json'); // SHOULD BE MOVED TO CLOUD.
+
+  const [isMapLoaded, setIsMapLoaded] = useState(false);
+  const mapContainer = useRef(null);
 
   useEffect(() => {
-    if (dataFetchedRef.current) return;
-    dataFetchedRef.current = true;
+    if (isMapLoaded) return;
+    setIsMapLoaded(true);
 
-    const map = new mapboxgl.Map({
-      accessToken:
-        'pk.eyJ1IjoibWFydGluYnJ1bGFuZCIsImEiOiJjbGFzMWszYzUxeGxhM3lvYnNoa3VvaDhwIn0.B51CuZmflun0olqPU9KvTA',
-      style: 'mapbox://styles/mapbox/streets-v12',
-      container: 'map', // container ID
-      center: [10, 64],
-      zoom: 4.3
-      //maxBounds: [[-20, 57], [35, 80]] // N.W, S.E Coordinates. === Norway.
+    const mapStyle =
+      'https://api.maptiler.com/maps/streets/style.json?key=get_your_own_OpIi9ZULNHzrESv6T2vL';
+
+    const initialState = {
+      lng: 10,
+      lat: 65,
+      zoom: 4
+    };
+
+    const map = new Map({
+      container: mapContainer.current,
+      style: mapStyle,
+      center: [initialState.lng, initialState.lat],
+      zoom: initialState.zoom
+    });
+
+    map.on('load', async function () {
+      const featureCollectionBorder = mapData['postnummeromrader.postnummeromradegrense']; // Draw border. https://maplibre.org/maplibre-gl-js-docs/example/geojson-line/
+      const featureCollectionFill = mapData['postnummeromrader.postnummeromrade']; // Fill area. https://maplibre.org/maplibre-gl-js-docs/example/geojson-polygon/
+
+      const sourceDataBorder = {
+        type: 'geojson',
+        data: featureCollectionBorder
+      };
+
+      const sourceDataFill = {
+        type: 'geojson',
+        data: featureCollectionFill
+      };
+
+      const mapBorders = {
+        id: `outline`,
+        type: 'line',
+        source: `route2`,
+        layout: {
+          'line-join': 'round',
+          'line-cap': 'round'
+        },
+        paint: {
+          'line-color': '#888',
+          'line-width': 1
+        }
+      };
+
+      const mapFill = {
+        id: `polygons`,
+        type: 'fill',
+        source: `route1`,
+        layout: {},
+        paint: {
+          'fill-color': '#088',
+          'fill-opacity': 0.2
+        }
+      };
+
+      map.addSource(`route2`, sourceDataBorder);
+      map.addSource(`route1`, sourceDataFill);
+
+      map.addLayer(mapBorders);
+      map.addLayer(mapFill);
+
+      map.on('click', 'polygons', (e) => {
+        new maplibregl.Popup()
+          .setLngLat(e.lngLat)
+          .setHTML(
+            '<h3>' +
+              'Valgt område: ' +
+              '</h3>' +
+              '<p>' +
+              'Postnummer: ' +
+              e.features[0].properties['postnummer'] +
+              '</p>' +
+              '<p>' +
+              'Poststed: ' +
+              e.features[0].properties['poststed'] +
+              '</p>' +
+              '<p>' +
+              'Kommune: ' +
+              e.features[0].properties['kommune'] +
+              '</p>'
+          )
+          .setMaxWidth('300px')
+          .addTo(map);
+      });
     });
   }, []);
 
   return (
     <>
-      <div id="map" className="absolute w-full h-full" />
-
+      <div ref={mapContainer} className="absolute top-0 right-0 bottom-0 left-0" />
       <Sidebar headerTitle={'Utforsker'} headerButton={<Link href={'/'}>{'<--'}</Link>}></Sidebar>
     </>
   );
