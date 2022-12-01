7094533f7d4bbc501fad4a5c64cfc345a8f86dd2
Emil Flatøy Hodneland <emil.flatoyhodneland@tietoevry.com>
2022-12-01 15:16:05 +0100

    Added animation for the dropdown

--- web/components/Navbar/navbar.js
+++ web/components/Navbar/navbar.js
@@ -65,8 +65,10 @@ const Navbar = ({ headerPaths }) => {
 
         <div
           className={
-            (isMenuOpen ? 'animate-fadeIn display: flex ' : 'hidden ') +
-            'flex-col md:flex-row md:flex w-full md:w-fit md:items-center h-max'
+            (isMenuOpen
+              ? ' animate-fadeIn display: flex flex-grow mb-20 pb-14 transition-all transform duration-300  max-h-0 '
+              : ' flex-grow mb-0 duration-300 max-h-0 ') +
+            'flex-col md:flex-row md:flex w-full md:w-fit md:items-center h-max md:flex-grow-0 md:visible'
           }
           id="mobile-dropdown-menu">
           {menuLinks &&
@@ -74,9 +76,12 @@ const Navbar = ({ headerPaths }) => {
             menuLinks.map((item, index) => (
               <CustomLink to={item.slug} key={index}>
                 <a
-                  className={`text-lg lg:inline-flex lg:w-auto w-full px-3 py-2 text-black ${
-                    currentRoute == item.slug ? 'font-bold' : 'font-medium'
-                  } items-center justify-center hover:text-heroblue md:flex-col`}>
+                  className={
+                    (isMenuOpen ? 'visible ' : 'hidden ') +
+                    ` text-lg lg:inline-flex lg:w-auto w-full px-3 py-2 text-black ${
+                      currentRoute == item.slug ? 'font-bold' : 'font-medium'
+                    } items-center justify-center hover:text-heroblue md:flex-col`
+                  }>
                   {item.title}
                 </a>
               </CustomLink>
